03e92fbfb2aca9b89aac90d0a4b3a5b6e1af28c5
Ben Fausch <ben@digitalbureau.com>
2018-02-13 11:31:09 -0700

    styling and mobile view

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log(`%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo />
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+
+YOU HAVE FOUND TEH SACRED CODE COW
+DONATE $30 CRYPTO FOR GOOD TIMES
+
+NO JS ON THIS PAGE, ONLY BAD MOFO CSS DAWG
+                `, "font-family:Helvetica; font-size:36px; color:#7FFF00;")
+
+  if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+      console.log("It looks like you're using IE and checking the console. Do you need assistance?")
+    }                
